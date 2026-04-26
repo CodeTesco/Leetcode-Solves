96b29abebf1c4028b08b8c0c0c8bf9c6b9e83bde
CodeTesco <tescosquad2007@gmail.com>
2026-04-26 11:36:53 +0100

    added latest solve

---
+++ Chronological/L52totalNQueens.py
@@ -0,0 +1,40 @@
+def totalNQueens(n):
+    count = 0
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    sol = [["." for _ in range(n)] for _ in range(n)]
+
+    def updateBoard(board, r, c, val):
+        for i in range(n):
+            board[r][i] += val
+            board[i][c] += val
+        
+        board[r][c] -= val
+
+        directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+        for dr, dc in directions:
+            curr_r, curr_c = r + dr, c + dc
+            while 0 <= curr_r < n and 0 <= curr_c < n:
+                board[curr_r][curr_c] += val
+                curr_r += dr
+                curr_c += dc
+
+    def backtrack(row):
+        nonlocal count
+        if row == n:
+            count += 1
+            return
+
+        for col in range(n):
+            if board[row][col] == 0:
+                sol[row][col] = "Q"
+                updateBoard(board, row, col, 1) 
+                
+                backtrack(row + 1)
+                
+                sol[row][col] = "."
+                updateBoard(board, row, col, -1) 
+
+    backtrack(0)
+    return count
+
+print(totalNQueens(1))
