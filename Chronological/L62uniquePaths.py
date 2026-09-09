b98cae854173eb7516709bbaa29b9aa4e5cea450
CodeTesco <tescosquad2007@gmail.com>
2026-09-09 10:03:18 +0100

    little update to recent solve-leetcode 62

--- Chronological/L62uniquePaths.py
+++ Chronological/L62uniquePaths.py
@@ -1,14 +1,14 @@
 def uniquePaths(m, n):
     map = dict()
     def numPaths(m, n):
-        if map.get(f"({m}, {n})") is not None:
-            return map[f"({m}, {n})"]
+        if map.get((m,n)) is not None:
+            return map[(m,n)]
         if m == 1 or n == 1:
             return 1
 
         sum = numPaths(m-1, n) + numPaths(m, n-1)
-        map[f"({m}, {n})"] = sum
+        map[(m,n)] = sum
         return sum
     return numPaths(m, n)
 
-print(uniquePaths(23, 12))
+print(uniquePaths(3, 7))
